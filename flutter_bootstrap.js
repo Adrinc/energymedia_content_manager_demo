81f378fed577fee0b6c2ac2ac7226dbd5a151142
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2026-01-15 21:41:51 -0800

    cambios en thumbnails upload mas testeo de fix de rehidratacion de el buscador

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2330323092"
+    serviceWorkerVersion: "3500215707"
   }
 });
